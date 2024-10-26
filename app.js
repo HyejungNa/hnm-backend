02f3bf3d347b330990f8ce6ca1c7d086cc1bf6d5
Hyejung Na <hyejung.na01@gmail.com>
2024-10-26 16:01:23 +1100

    Update backend uri

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const cors = require("cors");
 const indexRouter = require("./routes/index");
 const app = express();
 
-require("dotenv").config(); // 환경변수 가져오기
+require("dotenv").config(); // 환경변수(.env파일) 가져오기
 
 app.use(cors()); // CORS 미들웨어 설정
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //req.body를 객체로 인식하게해줌
 
 app.use("/api", indexRouter);
-const mongoURI = process.env.LOCAL_DB_ADDRESS; // 백엔드 DB 주소
+
+// 백엔드 DB 주소
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+// console.log("mongouri", MONGODB_URI_PROD);
+const mongoURI = MONGODB_URI_PROD;
+// const mongoURI = process.env.LOCAL_DB_ADDRESS;
+
 mongoose
   .connect(mongoURI) // { useNewUrlParser } : mongodb최신버전에서 기본값으로 설정되어있어 더이상 사용하지않음
   .then(() => console.log("mongoose connected"))
--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
 const userSchema = Schema(
   {
     email: { type: String, required: true, unique: true, lowercase: true },
@@ -9,6 +13,14 @@ const userSchema = Schema(
   },
   { timestamps: true }
 );
+
+userSchema.methods.generateToken = async function () {
+  const token = await jwt.sign({ _id: this.id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
 userSchema.methods.toJSON = function () {
   const obj = this._doc; // _doc은 현재 mongoose 문서에서 모든 데이터를 담고있는 객체를 가리킴
   delete obj.password; // 백엔드에서 프론토엔드로 갈때 패스워드 항상 제외
