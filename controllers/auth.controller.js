9fa989bdef381731c3308656379034375c7fc817
Hyejung Na <hyejung.na01@gmail.com>
2024-10-28 02:56:52 +1100

    Add token login logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,8 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const authController = {};
 
@@ -16,6 +19,22 @@ authController.loginWithEmail = async (req, res) => {
       }
     }
     throw new Error("invalid email or password");
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+// 중간에 사용되기에(미들웨어) next필요함
+authController.authenticate = async (req, res, next) => {
+  try {
+    const tokenString = req.headers.authorization;
+    if (!tokenString) throw new Error("Token not found");
+    const token = tokenString.replace("Bearer ", "");
+    jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+      if (error) throw new Error("invalid token");
+      req.userId = payload._id;
+    });
+    next(); // 미들웨어에서의 다음 함수로 넘어가게끔해줌 (user.api.js파일안 userController.getUser함수로 넘어가게됨)
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
--- routes/user.api.js
+++ routes/user.api.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const userController = require("../controllers/user.controller");
 const router = express.Router();
+const userController = require("../controllers/user.controller");
+const authController = require("../controllers/auth.controller");
 
 // 회원가입
 router.post("/", userController.createUser);
+router.get("/me", authController.authenticate, userController.getUser); // 토큰이 valid한 토큰인지(1.expire,2.유저찾기가능한지), 이 token값을 가지고 유저를 찾아서 리턴 (미들웨어 구조)
 
 module.exports = router;
