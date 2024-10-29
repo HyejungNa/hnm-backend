a59408b6824f6ded162de5dc93c9e4afcb753d06
Hyejung Na <hyejung.na01@gmail.com>
2024-10-29 22:30:20 +1100

    Update comments

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -33,18 +33,22 @@ userController.getUser = async (req, res) => {
     const { userId } = req;
     const user = await User.findById(userId);
 
-    // 유저가 찾아지지않을때조건을 먼저찾아 중복으로 요청하지않도록 변경
+    // 유저가 발견되지않았을때를 먼저찾아 404응답을 보내고 즉시종료해서 이후코드가 실행되지않아 중복으로 요청하지않도록 변경
     if (!user) {
       throw new Error("can not find user");
     }
+    // 유저가 발견된 경우에만 성공 응답을 보냄
     res.status(200).json({ status: "success", user });
 
-    // 계속 중복으로 요청을해서 ERR_HTTP_HEADERS_SENT에러발생시킴 (* only one response is sent per request!)
     // if (user) {
+    ///// 유저가 발견된 경우 성공 응답을 보냄
     //   res.status(200).json({ status: "success", user });
     // }
-    // throw new Error("invalid token");
+    ///// 유저가 발견되지 않은 경우에 대한 처리가 없음
+    // throw new Error("invalid token"); // 여기서 에러가 발생
+    // (* only one response is sent per request!)
   } catch (error) {
+    // 에러 발생 시 400 응답을 보내는 처리
     res.status(400).json({ status: "error", error: error.message });
   }
 };
