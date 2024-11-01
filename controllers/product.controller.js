682b7eb83c2202c12c6c8096a4299626932e9791
Hyejung Na <hyejung.na01@gmail.com>
2024-11-02 02:16:03 +1100

    Add product editing to admin page

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -91,4 +91,30 @@ productController.getProducts = async (req, res) => {
   }
 };
 
+productController.updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.id; // product.api에서 이미 id값을 받아오고있음
+    const {
+      sku,
+      name,
+      size,
+      image,
+      price,
+      description,
+      category,
+      stock,
+      status,
+    } = req.body;
+
+    const product = await Product.findByIdAndUpdate(
+      { _id: productId },
+      { sku, name, size, image, price, description, category, stock, status },
+      { new: true } // 업데이트한 후 새로운값을 반환해줌
+    );
+    if (!product) throw new Error("item doesn't exist");
+    res.status(200).json({ status: "success", data: product });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
 module.exports = productController;
