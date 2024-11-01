129f98ef26666b0e6bf27d24fc7fd4be8a680617
Hyejung Na <hyejung.na01@gmail.com>
2024-11-02 02:16:16 +1100

    Add product editing to admin page

--- routes/product.api.js
+++ routes/product.api.js
@@ -3,7 +3,7 @@ const authController = require("../controllers/auth.controller");
 const productController = require("../controllers/product.controller");
 const router = express.Router();
 
-// 미들웨어
+// 상품 생성하기 (미들웨어사용 - 상품생성전 admin체크 필수)
 router.post(
   "/",
   authController.authenticate, // 토큰값 받기
@@ -13,5 +13,13 @@ router.post(
 
 // 상품 보여주기
 router.get("/", productController.getProducts);
+
+// 상품 수정하기 (상품 수정전 admin 체크필수)
+router.put(
+  "/:id",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  productController.updateProduct
+);
 
 module.exports = router;
