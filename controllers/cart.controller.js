83a2c8268e59174c6ec7d7b3875a7a0c71fe58d3
Hyejung Na <hyejung.na01@gmail.com>
2024-11-03 22:25:17 +1100

    Implement cart item logic: fetch, delete, update quantity, and get total count

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -31,4 +31,86 @@ cartController.addItemToCart = async (req, res) => {
   }
 };
 
+// 카트 아이템 가져오기
+cartController.getCart = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId }).populate({
+      // populate사용시 외래키를통해 추가정보를 같이들고올수있음
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+      },
+    });
+    res.status(200).json({ status: "success", data: cart.items });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+// 카트 아이템 삭제하기
+cartController.deleteCartItem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId });
+    cart.items = cart.items.filter((item) => !item._id.equals(id));
+
+    await cart.save();
+    res.status(200).json({ status: 200, cartItemQty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+// 카트 아이템 수정하기
+cartController.updateQty = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    const { id } = req.params;
+    const { qty } = req.body;
+
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+      },
+    });
+
+    if (!cart) {
+      const error = new Error("There is no cart for this user.");
+      error.status = 400;
+      return next(error);
+    }
+
+    const index = cart.items.findIndex((item) => item._id.equals(id));
+
+    if (index === -1) {
+      const error = new Error("Can not find item.");
+      error.status = 400;
+      return next(error);
+    }
+    cart.items[index].qty = qty;
+    await cart.save();
+    res.status(200).json({ status: "success", data: cart.items });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+// 카트의 전체아이템 총 갯수 가져오기
+cartController.getCartQty = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) throw new Error("There is no cart!");
+    res.status(200).json({ status: 200, qty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = cartController;
