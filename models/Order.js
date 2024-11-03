afe50ed0321536ca3deda7b08c3b9e24d8b8c7fd
Hyejung Na <hyejung.na01@gmail.com>
2024-11-04 03:13:00 +1100

    Add comments

--- models/Order.js
+++ models/Order.js
@@ -4,16 +4,17 @@ const Product = require("./Product");
 const Schema = mongoose.Schema;
 const orderSchema = Schema(
   {
-    userId: { type: mongoose.ObjectId, ref: User, required: true },
+    userId: { type: mongoose.ObjectId, ref: User, required: true }, // 토큰값 사용
     status: {
       type: String,
-      default: "preparing",
+      default: "preparing", // order받으면 우선 기본값 preparing으로 되도록 설정
     },
-    totalPrice: { type: Number, required: true, default: 0 },
-    shipTo: { type: Object, required: true },
-    contact: { type: Object, required: true },
-    orderNum: { type: String },
+    totalPrice: { type: Number, required: true, default: 0 }, // payment페이지에서 받을정보
+    shipTo: { type: Object, required: true }, // payment페이지에서 받을정보: address + city + zip
+    contact: { type: Object, required: true }, // payment페이지에서 받을정보: last & first name + contact number
+    orderNum: { type: String }, // 유저에게 보여주는 주문번호 (오더생성후 만들어진번호)
     items: [
+      // 여러개의 아이템을 주문할수있기에 배열에받아주기
       {
         productId: { type: mongoose.ObjectId, ref: Product, required: true },
         price: {
