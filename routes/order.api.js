79a64a9f02f2dce90973f82d539cc09632bbc5ee
Hyejung Na <hyejung.na01@gmail.com>
2024-11-08 06:34:37 +1100

    Feat(admin): Implement order list and update feature

--- routes/order.api.js
+++ routes/order.api.js
@@ -7,15 +7,18 @@ const router = express.Router();
 // 남이아닌 본인만 주문할수있어야하기에 미들웨어로 authenticate 필요
 router.post("/", authController.authenticate, orderController.createOrder);
 
+// 개인유저가 주문한 내역 확인하는 my order
 router.get("/me", authController.authenticate, orderController.getOrder);
 
-// router.get("/", authController.authenticate, orderController.getOrderList);
-// router.put(
-//   "/:id",
-//   authController.authenticate,
-//   authController.checkAdminPermission,
-//   orderController.updateOrder,
-//   orderController.getOrderList
-// );
+// admin page에서 order list
+router.get("/", authController.authenticate, orderController.getOrderList);
+
+router.put(
+  "/:id",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  orderController.updateOrder,
+  orderController.getOrderList
+);
 
 module.exports = router;
