46a02914e7bc027d3e2ebc99eca7cf27d3368bc4
Hyejung Na <hyejung.na01@gmail.com>
2024-11-08 02:09:26 +1100

    feat: Implement logic to fetch order history(my order)

--- routes/order.api.js
+++ routes/order.api.js
@@ -1,3 +1,4 @@
+/* <깃헙 리포복구> */
 const express = require("express");
 const authController = require("../controllers/auth.controller");
 const orderController = require("../controllers/order.controller");
@@ -5,5 +6,16 @@ const router = express.Router();
 
 // 남이아닌 본인만 주문할수있어야하기에 미들웨어로 authenticate 필요
 router.post("/", authController.authenticate, orderController.createOrder);
+
+router.get("/me", authController.authenticate, orderController.getOrder);
+
+// router.get("/", authController.authenticate, orderController.getOrderList);
+// router.put(
+//   "/:id",
+//   authController.authenticate,
+//   authController.checkAdminPermission,
+//   orderController.updateOrder,
+//   orderController.getOrderList
+// );
 
 module.exports = router;
